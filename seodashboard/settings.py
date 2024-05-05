989a7f3c362669f6d2fe41b1a7bc676bfe1139b6
Noë Snaterse <noe@wiredpea.com>
2024-05-05 10:31:42 +0200

    Added Tailwind as an HTML / CSS framework

--- seodashboard/settings.py
+++ seodashboard/settings.py
@@ -27,6 +27,9 @@ SECRET_KEY = "django-insecure-f*t!sgl()-p42v($ou(doj3-6k(n_&#cs2ur#88ah)gkj0-n-l
 DEBUG = True
 
 ALLOWED_HOSTS: list[str] = []
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -39,6 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "tailwind",
+    "theme",
+    "django_browser_reload",
+    "compressor",
     "users",
 ]
 
@@ -50,6 +57,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "seodashboard.urls"
@@ -57,7 +65,7 @@ ROOT_URLCONF = "seodashboard.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -111,6 +119,13 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# Tailwind
+TAILWIND_APP_NAME = "theme"
+
+COMPRESS_ROOT = BASE_DIR / "static"
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
 
 
 # REST framework
